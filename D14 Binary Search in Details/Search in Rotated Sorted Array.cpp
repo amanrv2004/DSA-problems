fceaf8090092f16c215e683b88ff8e66ff367500
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-14 12:48:30 +0530

    all

---
+++ D14 Binary Search in Details/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+        int arr[] = {4,5,1,2,3};
+        int n = sizeof(arr)/sizeof(arr[0]);
+        int start = 0;
+        int end = n - 1;
+        int pivot = arr[0];
+        int index;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(arr[mid]>=pivot){
+                start = mid +1;
+            }
+            else{
+                index = mid;
+                end = mid-1;
+            }
+        }
+        cout << "K = " << index << endl;
+
+    return 0;
+}
